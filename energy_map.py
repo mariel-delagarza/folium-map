577da12b373d4e9e67ea758ddf31c85390d31d13
mariel-delagarza <mariel.code@gmail.com>
2023-04-27 12:16:58 -0400

    move making choropleth to module

--- energy_map.py
+++ energy_map.py
@@ -7,31 +7,24 @@ import geopandas as gpd
 import folium
 from folium.features import GeoJsonTooltip
 import geojson as geoj
+import choropleth_layer
 
-# fetch google sheet
-res = requests.get('https://sheets.googleapis.com/v4/spreadsheets/19uN6jYmrvuXwwrcOnwpTlQSJAEYP9gH6TQdq3Jn4z94/values/Commodity_Exports!A1:J?key=AIzaSyAImbihK2tiRewSFzuJTF_lcgPlGSr7zcg')
-# turn google sheet into json
-response = json.loads(res.text)
+# get csv from google sheet, turn into pandas dataframe
+data = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRUKtBM0I1eo7HVni3eWJOxNSidviWa7wN2JbTHO4tizb5ffIG0_f1BsYNp32eCXkKiSE9nW67WT_ud/pub?gid=1518327281&single=true&output=csv')
+pd.DataFrame(data).to_csv('commodity_exports.csv', index=False)
+commodity_exports_df = pd.DataFrame(data)
 
-# get json values, turn into CSV
-arr = np.asarray(response['values'])
-pd.DataFrame(arr).to_csv('commodity_exports.csv', index=False, header=False)
-commodity_exports_df = pd.read_csv('./commodity_exports.csv')
-
-print(commodity_exports_df.info())
-
-# Read the geojson file using geopandas
+# Read the geojson file using geopandas, only select state_id and geometry
 geojson = gpd.read_file('./states.geojson')
-# only select state_id and geometry
 geojson = geojson[['state_id', 'geometry']]
-print(geojson.head())
 
+# store geojson in variable to use in map
 with open('./states.geojson') as f:
     gj = geoj.load(f)
 
+# join CSV + geo data
 df_final = geojson.merge(commodity_exports_df, left_on="state_id",
                          right_on="state_id", how="outer")
-df_final = df_final[~df_final['geometry'].isna()]
 
 us_map = folium.Map(location=[40, -96],
                     zoom_start=4, tiles=None, overlay=False)
@@ -44,39 +37,12 @@ custom_scale1 = (df_final['coal'].quantile(
 custom_scale2 = (df_final['rpp'].quantile(
     (0, 0.25, 0.5, 0.75, 1))).tolist()
 
-coal = folium.Choropleth(
-    geo_data=gj,
-    data=df_final,
-    # Here we tell folium to get the state_ids and plot new_cases_7days metric for each county
-    columns=['state_id', 'coal'],
-    # Here we grab the geometries/county boundaries from the geojson file using the key 'coty_code' which is the same as county fips
-    key_on='feature.properties.state_id',
-    threshold_scale=custom_scale1,  # use the custom scale we created for legend
-    fill_color='Greys',
-    nan_fill_color="White",  # Use white color if there is no data available for the county
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    # title of the legend
-    legend_name='2020 Exports of Coal to Canada',
-    highlight=True,
-    line_color='black').geojson.add_to(fg1)
+coal = choropleth_layer.make_choropleth(gj, df_final, [
+                                        'state_id', 'coal'], 'feature.properties.state_id', custom_scale1, 'Greys', 'White', '2020 Exports of Coal to Canada', fg1)
 
-rpp = folium.Choropleth(
-    geo_data=gj,
-    data=df_final,
-    # Here we tell folium to get the state_ids and plot new_cases_7days metric for each county
-    columns=['state_id', 'rpp'],
-    # Here we grab the geometries/county boundaries from the geojson file using the key 'coty_code' which is the same as county fips
-    key_on='feature.properties.state_id',
-    threshold_scale=custom_scale2,  # use the custom scale we created for legend
-    fill_color='Blues',
-    nan_fill_color="White",  # Use white color if there is no data available for the county
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    # title of the legend
-    legend_name='2020 Exports of Raw Petroleum Products to Canada',
-    highlight=True,
-    line_color='black').geojson.add_to(fg2)
+rpp = choropleth_layer.make_choropleth(gj, df_final, ['state_id', 'rpp'], 'feature.properties.state_id',
+                                       custom_scale2, 'Blues', 'White', '2020 Exports of Raw Petroleum Products to Canada', fg2)
+
 folium.features.GeoJson(
     data=df_final,
     name='2020 Exports of Coal to Canada',
@@ -134,3 +100,4 @@ folium.LayerControl(collapsed=False).add_to(us_map)
 us_map
 
 us_map.save('index.html')
+print('success')
